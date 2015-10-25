7d275ab56f6339a599715dc864a34cde5d160269
Eirik S. Morland <eirik@morland.no>
2015-10-25 07:20:41 +0100

    Add current version.

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
+'use strict';
 var tessel = require('tessel');
-var climatelib = require('climate-si7005');
 var ambientlib = require('ambient-attx4');
+var ambient = ambientlib.use(tessel.port.A);
+var climateLib = require('climate-si7005');
+var climate = climateLib.use(tessel.port.B);
 var wifi = require('wifi-cc3000');
-var url = require('url');
-var http = require('http');
 var config = require('./config');
+var interval = config.interval;
+var plopId = config.plopId;
+var plopHeader = config.plopHeader;
+var tempId = config.tempId;
+var tempHeader = config.tempHeader;
+var alreadyTrying = false;
+// Change this to "true" to display more verbose output.
+var debug = false;
+var logger = require('./src/logger')(debug);
+var Queue = require('./src/queue');
 
-function logger(msg) {
-  //console.log(new Date().toString() + ': ' + msg);
-}
+config.errorCallback = function() {
+  tessel.reset_board();
+};
+var processQueue = new Queue(config);
 
 var tryToConnect = function() {
+  if (alreadyTrying) {
+    return;
+  }
   if (wifi.connection()) {
     return;
   }
   if (wifi.isBusy()) {
-    setTimeout(tryToConnect, 10000);
     return;
   }
+  alreadyTrying = true;
   wifi.connect({
     ssid: config.ssid,
     password: config.password
   }, function(err, res) {
+    alreadyTrying = false;
     if (err) {
       logger('Wifi connect error');
       logger(err);
@@ -52,58 +68,39 @@ wifi.reset(function() {
   logger('Did wifi reset');
 });
 
-var interval = config.interval;
-var plopId = config.plopId;
-var tempId = config.tempId;
-
-function sendData(val, path) {
-  logger('Sending data to ' + path);
-  var opts = url.parse(config.url + path);
-  opts.method = 'POST';
-  var ended = false;
-  var req = http.request(opts, function(res) {
-    logger('STATUS: ' + res.statusCode + ' (path ' + path + ')');
-    res.setEncoding('utf8');
-    res.on('err', function() {
-      logger('Error on res');
-    });
-    res.on('data', function(c) {
-      logger('Path ' + path + ' has data: ' + c);
-    });
-    res.on('close', function() {
-      logger('Request closed for path ' + path);
-      ended = true;
-    });
-    res.on('end', function() {
-      logger('Request ended for path ' + path);
-      ended = true;
-    });
+function sendData(val, path, header) {
+  processQueue.append({
+    val: val,
+    path: path,
+    header: header
   });
-  setTimeout(function() {
-    if (!ended) {
-      logger('Timing out request. Will abort. Path is ' + path);
-      req.abort();
-    }
-  }, 15000);
-  req.on('error', function(e) {
-    logger('Problem with request on path ' + path);
-    logger(e.message);
-    setTimeout(tryToConnect, 10000);
-  });
-  req.write(JSON.stringify({value: val}));
-  req.end();
 }
 
-var climate = climatelib.use(tessel.port.C);
-var ambient = ambientlib.use(tessel.port.D);
-
-var tl = require('tessel-temp-logger')(climate, {interval: interval});
-var tp = require('tessel-plops-logger')(ambient, {interval: interval}, tessel);
-
-tl.start(function(d) {
-  sendData(d, tempId);
+logger('Starting timers');
+var tp = require('tessel-plops-logger')(ambient, {
+  interval: interval,
+  debug: debug
+}, tessel);
+var ttl = require('tessel-temp-logger')(climate, {
+  interval: interval,
+  debug: debug
 });
-// Start them not at the same time, to avoid too many sockets and such.
-tp.start(function(d) {
-  sendData(d, plopId);
+ttl.start(function(err, d) {
+  if (err) {
+    logger('Error from climate. Will restart board', err);
+    config.errorCallback();
+    return;
+  }
+  logger('Data from climate:', d);
+  sendData(d, tempId, tempHeader);
 });
+tp.start(function(err, d) {
+  if (err) {
+    logger('Error from ambient. Will restart board', err);
+    config.errorCallback();
+    return;
+  }
+  logger('Data from ambient:', d);
+  sendData(d, plopId, plopHeader);
+});
+processQueue.start();
